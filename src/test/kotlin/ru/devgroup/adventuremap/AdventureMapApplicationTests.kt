0288ad232631521d02da3b3eb40bbf817dfb3dcc
tanexc <artur.231456@gmail.com>
2023-11-20 02:41:59 +0300

    initial commit

---
+++ src/test/kotlin/ru/devgroup/adventuremap/AdventureMapApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.devgroup.adventuremap
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdventureMapApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
